914a769a4178788f063ac891671d407513e9ce65
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2022-05-19 20:32:38 -0300

    clase 8 matrices

---
+++ Clase8/Clase8/Program.cs
@@ -0,0 +1,78 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+Console.WriteLine("Ingrese las notas");
+
+int[] notas = new int[5];
+
+for(int i = 0; i < notas.Length; i++)
+{
+    Console.WriteLine($"Notas del alumno N° {i + 1}: ");
+    notas[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < notas.Length; i++)
+{
+    Console.WriteLine($"La nota del alumno N° {i + 1}: {notas[i]}");
+}
+Console.WriteLine("Ingrese las notas");
+
+int[] notas = new int[5];
+
+for(int i = 0; i < notas.Length; i++)
+{
+    Console.WriteLine($"Notas del alumno N° {i + 1}: ");
+    notas[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < notas.Length; i++)
+{
+    Console.WriteLine($"La nota del alumno N° {i + 1}: {notas[i]}");
+}
+*/
+
+//¿Cuál es la diferencia entre un vector y una matriz?
+
+Console.WriteLine("Ingrese las notas de los exámenes");
+
+Console.WriteLine("Ingrese la cantidad de alumnos");
+
+int cantidadAlumnos = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la cantidad de notas por alumnos");
+
+int cantidadNotas = int.Parse(Console.ReadLine());
+
+int[,] notas = new int[cantidadNotas,cantidadAlumnos];   
+
+int[] prueba = new int[5];
+
+//Obtener dimensión de matriz.
+int lengthFilas= notas.GetUpperBound(0)+1;
+int lengthColumnas = notas.GetUpperBound(1)+1;
+
+//¿De que forma recorremos una matriz?. Con un for anidado.
+
+
+//
+for (int columnas = 0; columnas < lengthColumnas; columnas++)
+{
+    Console.WriteLine($"Notas del alumno N°: {columnas + 1}: ");
+
+    for(int filas = 0; filas < lengthFilas; filas++)
+    {
+        Console.WriteLine($"Cargue la nota N° {filas +1}: ");
+        notas[filas,columnas] = int.Parse(Console.ReadLine());
+    }
+}
+
+for (int columnas = 0; columnas < lengthColumnas; columnas++)
+{
+    Console.WriteLine("======================================");
+    Console.WriteLine($"Notas del alumno N°: {columnas + 1}: ");
+
+    for (int filas = 0; filas < lengthFilas; filas++)
+    {
+        Console.Write($"la nota N° {filas + 1}: ");
+        Console.WriteLine(notas[filas,columnas]);
+    }
+}
---
+++ Tarea-Clase7/Tarea-Clase7/Program.cs
@@ -0,0 +1,54 @@
+﻿//Definir arreglo de 10 posiciones.
+int[] numeros = new int[10];
+
+//Completar arreglo.
+Console.WriteLine("Ingrese los valores");
+for (int i = 0; i < numeros.Length; i++)
+{
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+//Sumar todos los números.
+double suma = 0;
+for (int i = 0; i < numeros.Length; i++)
+{
+    suma += numeros[i];
+}
+
+//Obtener el valor mayor.
+int valorMayor = numeros[0];
+for (int i = 0; i < numeros.Length; i++)
+{
+    if (valorMayor < numeros[i])
+    {
+        valorMayor = numeros[i];
+    }
+}
+
+//Obtener el valor menor.
+int valorMenor = numeros[0];
+for (int i = 0; i < numeros.Length; i++)
+{
+    if (valorMenor > numeros[i])
+    {
+        valorMenor = numeros[i];
+    }
+}
+
+
+//Mostrar número ingresados.
+Console.WriteLine("Elementos ingresados");
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine(numeros[i]);
+}
+
+//Calcular promedio.
+double promedio = (suma / numeros.Length);
+
+//Mostrar resultados.
+Console.WriteLine("");
+Console.WriteLine($"Suma total = {suma}");
+Console.WriteLine($"Promedio = {promedio}");
+Console.WriteLine($"Mayor valor = {valorMayor}");
+Console.WriteLine($"Menor valor = {valorMenor}");
