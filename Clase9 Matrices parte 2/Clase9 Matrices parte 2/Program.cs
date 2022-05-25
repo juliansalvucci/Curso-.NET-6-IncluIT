55b367fcec36bc35a0e5906f5650111b53a386fd
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2022-05-24 21:39:57 -0300

    clase 9

---
+++ Clase9 Matrices parte 2/Clase9 Matrices parte 2/Program.cs
@@ -0,0 +1,80 @@
+﻿// Array de n nros aleatorios.
+//Insertar un valor en una determinada posición.
+/*
+Console.WriteLine("Ingrese el tamaño deseado para e listado");
+
+//Cantidad de datos que va a tener el array.
+int n = int.Parse(Console.ReadLine());
+
+int[] vector = new int[n];
+
+Random aleatorizador = new Random();
+
+for (int i = 0; i < n; i++)
+{
+    vector[i] = aleatorizador.Next(100); //Next() devuelve un número entero positivo aleatorio, Max 100.
+}
+
+int contador = 0;
+
+//Por cada elemento.
+foreach (int i in vector)
+{
+    contador++;
+    Console.WriteLine(contador + " : " + i);
+}
+
+Console.WriteLine();
+Console.WriteLine("Ingrese posición a modificar: ");
+
+int posicionAModificar = int.Parse(Console.ReadLine());
+
+if(posicionAModificar > vector.Length || posicionAModificar < 0)
+{
+    Console.WriteLine("Índice incorrecto");
+}
+else
+{
+    Console.WriteLine("Ingrese nuevo valor");
+    vector[posicionAModificar - 1] = int.Parse(Console.ReadLine());
+
+    //Por cada elemento.
+    foreach (int i in vector)
+    {
+        contador++;
+        Console.WriteLine(contador + " : " + i);
+    }
+}
+*/
+
+int[] primos = new int[100];
+
+int posicion = 0;
+int aux = 0;
+int numero = 1;
+
+while(posicion < primos.Length)
+{
+    aux = 0;
+    for(int i = 1; i <= numero; i++)
+    {
+        if(numero % i == 0) {
+            aux++;
+        }
+    }
+    if (aux == 2) //Indica que el número tiene 2 divisores, el 1 y si mismo.
+    {
+        primos[posicion] = numero;
+        posicion++;
+    }
+    numero++;
+}
+
+foreach (int i in primos)
+{
+    Console.WriteLine(i);   
+}
+
+Console.ReadKey();
+
+//¿Cómo le determinado el tipo de datos a las listas? entre comillas angulares <>.
---
+++ TareaClase8/TareaClase8/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.WriteLine("Ingrese la cantidad de filas");
+
+int nroFilas = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la cantidad de columnas");
+
+int nroColumnas = int.Parse(Console.ReadLine());
+
+//Matríz de numeros.
+int[,] numeros = new int[nroFilas, nroColumnas];
+
+//Matriz unidimensional o Vector de promedios.
+double[] promedios = new double[nroColumnas];
+
+//Definición de dimensiones de la matriz bidimensional para utilizar en los ciclos.
+int lengthFilas = numeros.GetUpperBound(0) + 1;
+int lengthColumnas = numeros.GetUpperBound(1) + 1;
+
+//Ciclo para cargar datos en matriz.
+for (int columnas = 0; columnas < lengthColumnas; columnas++)
+{
+    for (int filas = 0; filas < lengthFilas; filas++)
+    {
+        Console.WriteLine($"Cargue el valor {filas + 1}: ");
+        numeros[filas, columnas] = int.Parse(Console.ReadLine());
+    }
+}
+
+//Ciclo para mostrar datos de la matriz.
+for (int columnas = 0; columnas < lengthColumnas; columnas++)
+{ 
+    for (int filas = 0; filas < lengthFilas; filas++)
+    {
+        Console.Write(numeros[filas, columnas]);
+    }
+}
+
+int suma = 0;
+
+for (int columnas = 0; columnas < lengthColumnas; columnas++)
+{
+    for (int filas = 0; filas < lengthFilas; filas++)
+    {
+
+    }
+}
+
+
+
