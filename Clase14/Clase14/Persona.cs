31fb0bb748951767fd78fc169b6e4d6f92811943
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2022-06-12 22:32:34 -0300

    clase 14

---
+++ Clase14/Clase14/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase14
+{
+    internal class Persona
+    {
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
+
+        //ctor + tab y crea el constructor.
+        public Persona()
+        {
+            Nombre = "Julián";
+            Apellido = "Salvucci";
+        }
+    }
+}
---
+++ Clase14/Clase14/Program.cs
@@ -0,0 +1,19 @@
+﻿// Destrctor vacia el espacio de memoria que ocupa el objeto.
+
+using Clase14;
+
+var personas = new List<Persona>();
+
+for(int i = 0; i < 100000000; i++)
+{
+    var P = new Persona(); //la ultima persona creada queda en el heap, desplazando el resto al stack.
+    personas.Add(P);
+}
+  
+
+
+//2 Secciones de memoria
+//En heap(montón) viven los objetos.
+// El garbage colector toma los objetos no referenciados y llama a su destructor.
+
+//Pregunta 1 ¿Qué es el garbage collector?
