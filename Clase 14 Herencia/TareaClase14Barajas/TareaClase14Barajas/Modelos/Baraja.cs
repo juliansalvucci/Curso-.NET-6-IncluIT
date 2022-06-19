89c1b384f717638f98f072b526c56b0f19513f36
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2022-06-19 19:00:39 -0300

    .

---
+++ Clase 14 Herencia/TareaClase14Barajas/TareaClase14Barajas/Modelos/Baraja.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaClase14Barajas.Modelos
+{
+    public class Baraja
+    {
+        public List<Carta> Cartas { get; set; }
+
+        public void Barajar()
+        {
+
+        }
+
+        public Carta SiguienteCarta()
+        {
+            return new Carta();
+        }
+
+        public int CartasDisponibles()
+        {
+            return Cartas.Capacity;
+        }
+
+        public void DarCartas(int cantidad)
+        {
+
+        }
+
+        public void CartasMonton()
+        {
+
+        }
+
+        public void MostrarBaraja()
+        {
+
+        }
+    }
+
+}
---
+++ Clase 14 Herencia/TareaClase14Barajas/TareaClase14Barajas/Modelos/Carta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TareaClase14Barajas
+{
+    public class Carta
+    {
+        public int NumeroCarta { get; set; }
+        public string Palo { get; set; }
+    }
+}
